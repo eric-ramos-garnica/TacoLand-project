f5a54565893987af8ee8bf88b5e183fff6059ea7
eric-ramos-garnica <ericramoshackbright@gmail.com>
2023-04-28 14:52:19 -0700

    fixed edit button for name

--- static/js/user_profile/user_profile_name.js
+++ static/js/user_profile/user_profile_name.js
@@ -1,9 +1,17 @@
-const cancel_button_name = document.getElementById("cancel-edit-name")
-cancel_button_name.addEventListener('click',(e)=>{
-    document.getElementById("first-last-name").removeAttribute('style');
-    document.getElementById("edit-form-name").setAttribute('style','display: none;');
-    e.preventDefault();
-    e.target.setAttribute('style','display: none;');
-    edit_button_name.removeAttribute("style");
-});
+const edit_button_name = document.getElementById("edit-button-name")
+        edit_button_name.addEventListener('click',(e)=>{
+            e.target.setAttribute('style','display:none;');
+            document.getElementById("edit-form-name").removeAttribute('style');
+            document.getElementById("first-last-name").setAttribute('style','display:none;');
+            document.getElementById("cancel-edit-name").removeAttribute("style");
+        });
+
+       const  cancel_button_name = document.getElementById("cancel-edit-name")
+        cancel_button_name.addEventListener('click',(e)=>{
+            document.getElementById("first-last-name").removeAttribute('style');
+            document.getElementById("edit-form-name").setAttribute('style','display: none;');
+            e.preventDefault();
+            e.target.setAttribute('style','display: none;');
+            edit_button_name.removeAttribute("style");
+        });
 
